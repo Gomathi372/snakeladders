105b6d29ce853b800b4081c3fd2467695cf8fed9
Gomathi372 <71174577+Gomathi372@users.noreply.github.com>
2020-09-12 16:05:10 +0530

    mycaptain

---
+++ mycaptain product of float.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	double num1;
+	double num2;
+	double total;
+	cout<<"enter first number:";
+	cin>>num1;
+	cout<<"enter second number:";
+	cin>>num2;
+	total=num2*num1;
+	cout<<"total is :"<<total;
+	return 0;
+}
